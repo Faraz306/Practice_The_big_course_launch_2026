c676807142be77ea977e737057cbb4270ccc304c
Faraz <farazmoin.uk@gmail.com>
2026-03-27 06:45:56 -0400

    Bye, Changer 2nd

--- pages/Learn Python for free from us!.py
+++ pages/Learn Python for free from us!.py
@@ -3,7 +3,8 @@ import streamlit as st
 st.title("Learn Python for Free!")
 st.info("The YF™ Official Course is currently in development.")
 
-st.link_button("Send us an Email", "mailto:farazyamaan@gmail.com", help="Click to open your email app", key="email_btn_01")
+# --- THE FIX ---
+st.link_button("Send us an Email", "mailto:farazyamaan@gmail.com")
 
 st.write("Or manually email us at: **farazyamaan@gmail.com**")
 
